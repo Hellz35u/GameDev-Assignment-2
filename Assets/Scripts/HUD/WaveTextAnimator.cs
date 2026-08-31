504124a813a409daa543e152ae524b1d7f6fa965
Maor Buchbut <hellz35u@gmail.com>
2026-09-01 00:35:56 +0300

    finished wave animation inOutBounce and create ScoreEvent script

--- Assets/Scripts/HUD/WaveTextAnimator.cs
+++ Assets/Scripts/HUD/WaveTextAnimator.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
-
+using TMPro;
+using DG.Tweening;
+using System.Collections;
+using System;
 public class WaveTextAnimator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] TextMeshProUGUI waveText;
+    private Tween tween;
+    private Vector3 originalScale;
+    [SerializeField] private float duration = 0.2f;
+    
+    
+
     void Start()
     {
-        
+        originalScale = transform.localScale;
+        StartCoroutine(PopingTime(WaveAnimation, 1));
     }
 
-    // Update is called once per frame
-    void Update()
+    private void WaveAnimation()
     {
-        
+        if (tween != null || tween.IsActive())
+        {
+            tween.Kill();
+        }
+        transform.localScale = Vector3.zero;
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(transform.DOScale(originalScale, duration).SetEase(Ease.InOutBounce));
+
+        tween = sequence;
+    }
+    private IEnumerator PopingTime(Action waveAnimation, int waitingTime)
+    {
+        waveText.enabled = false;
+        yield return new WaitForSeconds(waitingTime);
+        waveText.enabled = true;
+        waveAnimation();
+        yield return new WaitForSeconds(waitingTime);
+        waveText.enabled = false;
+    }
+    public void TextEditor(string text)
+    {
+        waveText.text = text;
     }
 }
