9184c626b453cbe0c53e7ddef021e97cd8d0f93b
ShlomiGanon <Shlomi1395@gmail.com>
2026-09-07 06:15:09 +0300

    improve player controller to handle events

--- Assets/Scripts/AI/NewPlayerController.cs
+++ Assets/Scripts/AI/NewPlayerController.cs
@@ -1,15 +1,21 @@
+using System;
 using UnityEngine;
 
 public class NewPlayerController : MonoBehaviour
 {
-    private CharacterActions characterActions;
-
+    private CharacterController characterActions;
+    private CharacterHealth characterHealth;
     void Awake()
     {
-        characterActions = GetComponent<CharacterActions>();
+        characterActions = GetComponent<CharacterController>();
         if (characterActions == null)
         {
             Debug.LogError("can't find CharacterActions script in this GameObject!");
+        }
+        characterHealth = GetComponent<CharacterHealth>();
+        if (characterHealth == null)
+        {
+            Debug.LogError("can't find CharacterHealth script in this GameObject!");
         }
     }
 
@@ -18,16 +24,33 @@ public class NewPlayerController : MonoBehaviour
         InputEvents.Move += characterActions.Move;
         InputEvents.Jump += characterActions.TryJump;
         InputEvents.Attack += characterActions.Attack;
-        PlayerEvents.Death += characterActions.Die;
-        PlayerEvents.TakeHit += characterActions.TakeHit;
+        GameEvents.CharacterDeath += OnCharacterDeath;
+        GameEvents.CharacterTakeHit += OnChracterTakeHit;
     }
+
+
+    private void OnCharacterDeath(GameObject deathGameObject)
+    {
+        if (deathGameObject != this.gameObject) return;
+
+        GameEvents.OnPlayerHealthUpdated(0, characterHealth.GetFullHealth());
+        
+    }
+    private void OnChracterTakeHit(GameObject hittedGameObject, int arg2)
+    {
+        if (hittedGameObject != this.gameObject) return;
+
+        GameEvents.OnPlayerHealthUpdated(characterHealth.GetHealth(), characterHealth.GetFullHealth());
+    }
+
+
 
     private void OnDisable()
     {
         InputEvents.Move -= characterActions.Move;
         InputEvents.Jump -= characterActions.TryJump;
         InputEvents.Attack -= characterActions.Attack;
-        PlayerEvents.Death -= characterActions.Die;
-        PlayerEvents.TakeHit -= characterActions.TakeHit;
+        GameEvents.CharacterDeath -= OnCharacterDeath;
+        GameEvents.CharacterTakeHit -= OnChracterTakeHit;
     }
 }
