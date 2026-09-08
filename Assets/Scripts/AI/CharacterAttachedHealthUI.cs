a5132ea567a380ef1f301f17f1085f075d0b0431
ShlomiGanon <Shlomi1395@gmail.com>
2026-09-08 07:05:18 +0300

    adding script name CharacterAttachedHealthUI to handle the UI the enemies have above them

---
+++ Assets/Scripts/AI/CharacterAttachedHealthUI.cs
@@ -0,0 +1,52 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterAttachedHealthUI : MonoBehaviour
+{
+    [SerializeField] private Slider healthBar;
+    [SerializeField] private TextMeshProUGUI healthPointsText;
+    private CharacterHealth characterHealth;
+
+    private void Awake()
+    {
+        characterHealth = GetComponent<CharacterHealth>();
+        if (characterHealth == null)
+        {
+            Debug.LogError("can't find CharacterHealth on this GameObject!");
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (characterHealth == null) return;
+        GameEvents.CharacterHealthChange += OnHealthChange;
+        UpdateHealthBar(characterHealth.GetHealth(), characterHealth.GetFullHealth());
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.CharacterHealthChange -= OnHealthChange;
+    }
+
+    private void OnHealthChange(GameObject character, int currentHealth, int fullHealth)
+    {
+        if (character != this.gameObject) return;
+        UpdateHealthBar(currentHealth, fullHealth);
+        UpdateHealthText($"{currentHealth}");
+    }
+
+    private void UpdateHealthText(string text)
+    {
+        if (healthPointsText == null) return;
+        healthPointsText.text = text;
+    }
+
+    private void UpdateHealthBar(int current, int full)
+    {
+        if (healthBar == null) return;
+        healthBar.maxValue = full;
+        healthBar.value = current;
+    }
+}
